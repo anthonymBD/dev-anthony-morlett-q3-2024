37f2e43ba7b3526b57786a2e4d8beae6435185b2
amorlett2021 <beardencreative1@gmail.com>
2024-08-01 19:00:43 -0500

    add javascript, css, fix error utm
    Had issues with UTM showing up correctly seems correct and fixed right now need to run more test, added javascript to reflect changes, javascript query seems to be working but may need upgrade this to save the utm_medium parameter the first time you read it
    so the message saves between page views if time allows

---
+++ assets/amBanner.js
@@ -0,0 +1,31 @@
+function currentUserUtmParameter() {
+
+
+    // Create a URLSearchParams object from the current page's query string
+    const searchParams = new URLSearchParams(window.location.search);
+
+
+    return sessionStorage.get('utm_medium') || searchParams.get('utm_medium');
+}
+
+function showDefaultMsg() {
+    const $fallback = document.querySelector('.message.fallback');
+
+    if ($fallback) $fallback.style.display = 'block'
+}
+console.log(showDefaultMsg() )
+
+function showMsgByParameter(parameterValue) {
+    const $msg = document.querySelector(`.message[data-utm="${parameterValue}"]`)
+
+    if ($msg) $msg.style.display = 'block';
+    else showDefaultMsg();
+}
+
+function determineMsgToShow() {
+    const utmSessionValue = currentUserUtmParameter();
+
+    showMsgByParameter(utmSessionValue)
+}
+
+determineMsgToShow();
